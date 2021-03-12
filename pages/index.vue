8ac1e06ffc7d96c08169ca04221bbdee14dbccd3
PawaOx4th <pawaox4th@gmail.com>
2021-03-12 23:27:05 +0700

    intial project

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div>
+      <p>illusion Guild</p>
+
+      <span>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+        numquam placeat qui reprehenderit voluptatibus tempore, eos obcaecati!
+        Possimus, dicta dolor.
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
